70a5e439fbcf2ab199550273c6b2955293e87de0
Shubham <99117973+ecapricorns@users.noreply.github.com>
2023-04-13 15:23:53 +0530

    Hey List...

---
+++ ArrayList.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class arrayList{
+    public static void main(String[] args) {
+        ArrayList <String> list = new ArrayList<>();
+        list.add("Shubham");
+        list.add("Arvind");
+        list.add("panchal");
+
+        //printing Arraylist values:
+        System.out.println("Your ArrayList is : "+list);
+    }
+}
---
+++ LinkedList.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+class linkedList{
+    public static void main(String[] args) {
+        LinkedList<String> al = new LinkedList<String>();
+        al.add("Shubham");
+        al.add("Panchal");
+        al.add("Vijay");
+        al.add("Aditi");
+        al.add("Vaishnavi");
+        System.out.println(al);
+    }
+}
